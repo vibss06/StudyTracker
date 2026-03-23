23ec92eaa93fd71874deeb02ccf44fce0a3ae33c
Vibhanshu Gajalkar <vibhanshugajalkar006@gmail.com>
2026-03-23 23:50:24 +0530

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/StudyTracker/",  
+
   plugins: [
     react(),
     tailwindcss(),
